af04cfa5cb6643ba0b83150c3181457d0caa5ba0
najandhamedua <hamed.najand@alumni.univ-avignon.fr>
2021-12-30 01:31:00 +0100

    bug fixed, extracting content improved

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -8,19 +8,19 @@ require 'pdf/reader'
 def search_after(pages, arg)
 	argInfo = getArgInfo(pages, arg) 	# get argument info
 	
-	if argInfo == false
-		return ""
+	if(argInfo[0].is_a? Integer)
+		idx = argInfo[1].index(arg) 		# get index of argument in one line within the page's lines
+		ln = argInfo[1].length				# get length of lines for one page
+		i = idx
+		line = ""
+		while i <= ln
+			i = i + 1
+			line += "#{argInfo[1][i]} "
+		end
+		return line
+	else
+		return false
 	end
-	
-	idx = argInfo[1].index(arg) 		# get index of argument in one line within the page's lines
-	ln = argInfo[1].length				# get length of lines for one page
-	i = idx
-	line = ""
-	while i <= ln
-		i = i + 1
-		line += "#{argInfo[1][i]} "
-	end
-	return line
 end
 
 # Searching content before an argument
@@ -29,19 +29,19 @@ end
 def search_before(pages, arg)
 	argInfo = getArgInfo(pages, arg)	# get argument info
 	
-	if argInfo == false
-		return ""
+	if(argInfo[0].is_a? Integer)
+		idx = argInfo[1].index(arg)			# get index of argument in one line within the page's lines
+		ln = argInfo[1].length				# get length of lines for one page
+		i = 0
+		line = ""
+		while i < idx
+			line += "#{argInfo[1][i]} "
+			i = i + 1
+		end
+		return line
+	else
+		return false
 	end
-	
-	idx = argInfo[1].index(arg)			# get index of argument in one line within the page's lines
-	ln = argInfo[1].length				# get length of lines for one page
-	i = 0
-	line = ""
-	while i < idx
-		line += "#{argInfo[1][i]} "
-		i = i + 1
-	end
-	return line
 end
 
 # Searching content after an argument until the last page
@@ -52,20 +52,20 @@ def search_to_end(pages, arg)
 	line += "#{search_after(pages, arg)} "
 
 	argInfo = getArgInfo(pages, arg)
-	
-	if argInfo == false
-		return ""
+
+	if(argInfo[0].is_a? Integer)
+		ln = pages.length
+
+		i = argInfo[0] + 1
+		while i < ln
+			line +=  "#{search_entire_page(pages, i)} "
+			i = i + 1
+		end
+
+		return line
+	else
+		return false
 	end
-
-	ln = pages.length
-
-	i = argInfo[0] + 1
-	while i < ln
-		line +=  "#{search_entire_page(pages, i)} "
-		i = i + 1
-	end
-
-	return line
 end
 
 # Searching content before an argument until the first page
@@ -76,21 +76,20 @@ def search_to_first(pages, arg)
 	
 	argInfo = getArgInfo(pages, arg)
 
-	if argInfo == false
-		return ""
+	if(argInfo[0].is_a? Integer)
+		idx = argInfo[1].index(arg)
+		i = 0
+		while i < argInfo[0]
+			line +=  "#{search_entire_page(pages, i)} "
+			i = i + 1
+		end
+
+		line += "#{search_before(pages, arg)} "
+
+		return line
+	else
+		return false
 	end
-
-	idx = argInfo[1].index(arg)
-
-	i = 0
-	while i < argInfo[0]
-		line +=  "#{search_entire_page(pages, i)} "
-		i = i + 1
-	end
-
-	line += "#{search_before(pages, arg)} "
-
-	return line
 end
 
 # get page index and lines for a specific argument
@@ -101,8 +100,6 @@ def getArgInfo(pages, arg)
 		lines = page.text.scan(/^.+/)			# catching all line on that page
 		if lines.include? arg					# determining if this argument exist on this page
 			return pages.index(page), lines		# return page index and page lines
-		else
-			return false
 		end
 	end
 end
@@ -113,28 +110,26 @@ end
 # @argTo: it should be a title name that we want to finish
 # This scenario is for catching content between two title
 def search_between(pages, argFrom, argTo)
-	argFromIdx = getArgInfo(pages, argFrom)
-	argToIdx = getArgInfo(pages, argTo)
+	argFromInfo = getArgInfo(pages, argFrom)
+	argToInfo = getArgInfo(pages, argTo)
 
-	if argFromIdx == false
-		return ""
-	elsif argToIdx == false
-		return ""
+	if argFromInfo[0].is_a? Integer and argToInfo[0].is_a? Integer
+		line = ""
+		line +=  "#{search_after(pages, argFrom)} "
+
+		i = argFromInfo[0] + 1
+		while i < argToInfo[0]
+			line +=  "#{search_entire_page(pages, i)} "
+			i = i + 1
+		end
+		
+
+		line +=  "#{search_before(pages, argTo)}"
+
+		return line
+	else
+		return false
 	end
-
-	line = ""
-	line +=  "#{search_after(pages, argFrom)} "
-
-	i = argFromIdx[0] + 1
-	while i < argToIdx[0]
-		line +=  "#{search_entire_page(pages, i)} "
-		i = i + 1
-	end
-	
-
-	line +=  "#{search_before(pages, argTo)}"
-
-	return line
 end
 
 # Get entrie page content, in this case we suppose that between title A and title B there are one to many pages
@@ -155,6 +150,8 @@ else
 	for arg in ARGV
 		if arg == "-x"
 			convertor = arg
+		elsif arg == "-t"
+			convertor = "-t"
 		else
 			# Start converting using PDF-Reader
 			PDF::Reader.open(arg) do |reader|
@@ -173,37 +170,26 @@ else
 				puts "\nWriting text to disk"
 				
 				# Extract the content
-				titre = ""
+				titre = search_before(reader.pages, "Abstract") || search_before(reader.pages, "A bstract") || search_before(reader.pages, "abstract") || search_before(reader.pages, "a bstract")
 				abstract = search_between(reader.pages, 'Abstract', 'Introduction')
-				auteur = ""
-				introduction = ""
-				corps = ""
+				auteur = File.basename(arg, ".pdf")
+				introduction = search_between(reader.pages, "Introduction", "2 ")
+				corps = search_between(reader.pages, "2 ", "Discussion")
 				discussion = search_between(reader.pages, 'Discussion', 'Conclusions')
 				conclusion = search_between(reader.pages, 'Conclusions', 'References')
 				biblio = search_to_end(reader.pages, "References")
 				
 				fileBasenmae = File.basename(arg)
 				if(convertor == "-x")
-					File.write fileBasenmae.split(".")[0]+'.xml', "<article>\n<preamble>" + (fileBasenmae != '' ? fileBasenmae : 'Not Detected!') + "</preamble>\n<titre>" + (titre != '' ? titre : 'Not Detected!') + "</titre>\n<auteur>" + (auteur != '' ? auteur : 'Not Detected!') + "</auteur>\n<abstract>" + (abstract != '' ? abstract : 'Not Detected!') + "</abstract>\n<introduction>" + (introduction != '' ? introduction : 'Not Detected!') + "</introduction>\n<corps>" + (corps != '' ? corps : 'Not Detected!') + "</corps>\n<conclusion>" + (conclusion != '' ? conclusion : 'Not Detected!') + "</conclusion>\n<discussion>" + (discussion != '' ? discussion : 'Not Detected!') + "</discussion>\n<biblio>" + (biblio != '' ? biblio : 'Not Detected!') + "</biblio>\n</article>"
+					File.write fileBasenmae.split(".")[0]+'.xml', "<article>\n<preamble>" + (fileBasenmae ? fileBasenmae : 'Not Detected!') + "</preamble>\n<titre>" + (titre ? titre : 'Not Detected!') + "</titre>\n<auteur>" + (auteur ? auteur : 'Not Detected!') + "</auteur>\n<abstract>" + (abstract ? abstract : 'Not Detected!') + "</abstract>\n<introduction>" + (introduction ? introduction : 'Not Detected!') + "</introduction>\n<corps>" + (corps ? corps : 'Not Detected!') + "</corps>\n<conclusion>" + (conclusion ? conclusion : 'Not Detected!') + "</conclusion>\n<discussion>" + (discussion ? discussion : 'Not Detected!') + "</discussion>\n<biblio>" + (biblio ? biblio : 'Not Detected!') + "</biblio>\n</article>"
 				elsif(convertor == "-t")
-					File.wite fileBasenmae.split(".")[0]+'.txt', "<article>\n<preamble>" + (fileBasenmae != '' ? fileBasenmae : 'Not Detected!') + "</preamble>\n<titre>" + (titre != '' ? titre : 'Not Detected!') + "</titre>\n<auteur>" + (auteur != '' ? auteur : 'Not Detected!') + "</auteur>\n<abstract>" + (abstract != '' ? abstract : 'Not Detected!') + "</abstract>\n<introduction>" + (introduction != '' ? introduction : 'Not Detected!') + "</introduction>\n<corps>" + (corps != '' ? corps : 'Not Detected!') + "</corps>\n<conclusion>" + (conclusion != '' ? conclusion : 'Not Detected!') + "</conclusion>\n<discussion>" + (discussion != '' ? discussion : 'Not Detected!') + "</discussion>\n<biblio>" + (biblio != '' ? biblio : 'Not Detected!') + "</biblio>\n</article>"
+					File.write fileBasenmae.split(".")[0]+'.txt', "<article>\n<preamble>" + (fileBasenmae ? fileBasenmae : 'Not Detected!') + "</preamble>\n<titre>" + (titre ? titre : 'Not Detected!') + "</titre>\n<auteur>" + (auteur ? auteur : 'Not Detected!') + "</auteur>\n<abstract>" + (abstract ? abstract : 'Not Detected!') + "</abstract>\n<introduction>" + (introduction ? introduction : 'Not Detected!') + "</introduction>\n<corps>" + (corps ? corps : 'Not Detected!') + "</corps>\n<conclusion>" + (conclusion ? conclusion : 'Not Detected!') + "</conclusion>\n<discussion>" + (discussion ? discussion : 'Not Detected!') + "</discussion>\n<biblio>" + (biblio ? biblio : 'Not Detected!') + "</biblio>\n</article>"
 				else
 					puts "ERROR: There is a problem to determin the first argument"
 					exit
 				end
 			end
 		end
-
-		
-		# # puts search_between(reader.pages, "Title 1", "Page 3 line 3")
-		# # puts search_to_first(reader.pages, "Page 1 line 2")
-
-		
-		# # Inserting to the Text file
-		
-		
-		# # File.write filename+'.xml', "<article>\n<preamble>" + filename + "</preamble>\n<titre>" + title + "</titre>\n<abstract>" + rows.join("") + "</abstract>\n</article>"
-		# end # pdf::Reader
 	end # ARGV
 
 	# code to time
